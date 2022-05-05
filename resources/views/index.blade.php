40905c7bfa92592cab495a191ecc7c4af4292a8b
tiago <tiagolemespalhano@gmail.com>
2022-05-05 02:39:45 -0300

    173. Criação do projeto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="crsf-token" content="{{ csrf_token() }}">
+	<title>Pagination</title>
+
+</head>
+
+
+
+<body>
+
+	<div class="container bg-dark">
+		TESTE
+	</div>
+
+
+
+
+	<script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
